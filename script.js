e2c0fd443610fe000323b3383bdb690cc2049e9a
Ryan Seddon <seddon.ryan@gmail.com>
2015-04-13 06:16:25 +1000

    Lesson 3.1 Fnal

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+ * Using jQuery to attach a click event to the
+ * button.
+ * You'll need to open your browser Dev Tools
+ * to see the output when you click the button.
+ */
+
+$('#btn').on('click', function() {
+  
+  alert(Modernizr.fullscreen);
+  
+});
