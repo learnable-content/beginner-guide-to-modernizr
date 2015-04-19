d822cfd11e6d7cdc8509f6d2af64133d023ad320
Ryan Seddon <seddon.ryan@gmail.com>
2015-04-20 06:12:42 +1000

    Lesson 3.5 final

--- script.js
+++ script.js
@@ -5,12 +5,24 @@
 
 $('#prefixed').on('click', function() {
 
-
+  $('#prefixedResult').text(
+    // Passing a single argument is for CSS
+    // e.g. Modernizr.prefixed('borderRadius')
+    Modernizr.prefixed(
+      $('#prefixedValue').val()
+    )
+  );
 
 });
 
 $('#prefixedCSS').on('click', function() {
 
-
+  $('#prefixedCSSResult').text(
+    // Only accepts a single argument
+    // e.g. Modernizr.prefixed('border-radius')
+    Modernizr.prefixedCSS(
+      $('#prefixedCSSValue').val()
+    )
+  );
 
 });
